b977c4ec37d8c0ebfccb61b34593a346f3390438
Vincent <vleclerc@inow.fr>
2018-07-02 17:09:46 +0200

    ajout du projet TodoList pour les WebApi

---
+++ WebApi/TodoList/TodoList/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TodoList.Controllers
+{
+    public class TestModel
+    {
+        public int ID { get; set; }
+        public string Commentaire { get; set; }
+    }
+
+    public class TestController : ApiController
+    {
+
+        // GET: api/test
+        public List<TestModel> GetTests()
+        {
+            List<TestModel> liste = new List<TestModel>();
+            liste.Add(new TestModel { ID = 42, Commentaire = "la réponse" });
+            liste.Add(new TestModel { ID = 39, Commentaire = "température actuelle" });
+            liste.Add(new TestModel { ID = 98, Commentaire = "au hasard" });
+
+            return liste;
+        }
+
+
+    }
+}
