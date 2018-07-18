22107e6f9c132abd753cc2531145c0a1c4afca93
Vincent <vleclerc@inow.fr>
2018-07-18 15:38:07 +0200

    Ajout de validateurs perso, suppression de la colonne ConfirmedPassword.

---
+++ AspMVC/Roomy/Roomy/Models/Room.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class Room : BaseModel
+    {
+        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+        [StringLength(50)]
+        [Display(Name = "Libellé")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+        [Display(Name = "Nombre de place")]
+        [Range(0, 50)]
+        public int Capacity { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+        [Display(Name = "Tarif")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+        [StringLength(50)]
+        [Display(Name = "Libellé")]
+        public string Name { get; set; }
+
+
+    }
+}
