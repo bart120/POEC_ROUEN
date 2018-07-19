fa0ef0745b8690c7c10d46ab72699e1ace3a7b2b
Vincent <vleclerc@inow.fr>
2018-07-19 09:48:33 +0200

    ajout du scafold pour Rooms, et web.config pour vincent

---
+++ AspMVC/Roomy/Roomy/Areas/BackOffice/Controllers/RoomsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Roomy.Data;
+using Roomy.Models;
+
+namespace Roomy.Areas.BackOffice.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private RoomyDbContext db = new RoomyDbContext();
+
+        // GET: BackOffice/Rooms
+        public ActionResult Index()
+        {
+            var rooms = db.Rooms.Include(r => r.User);
+            return View(rooms.ToList());
+        }
+
+        // GET: BackOffice/Rooms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // GET: BackOffice/Rooms/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Lastname");
+            return View();
+        }
+
+        // POST: BackOffice/Rooms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Capacity,Price,Description,CreatedAt,UserID")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rooms.Add(room);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Lastname", room.UserID);
+            return View(room);
+        }
+
+        // GET: BackOffice/Rooms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Lastname", room.UserID);
+            return View(room);
+        }
+
+        // POST: BackOffice/Rooms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Capacity,Price,Description,CreatedAt,UserID")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(room).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.Users, "ID", "Lastname", room.UserID);
+            return View(room);
+        }
+
+        // GET: BackOffice/Rooms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: BackOffice/Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Room room = db.Rooms.Find(id);
+            db.Rooms.Remove(room);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
