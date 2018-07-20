836018ddae20c86590a87e1f9b45f75720dbba50
Vincent <vleclerc@inow.fr>
2018-07-20 11:52:50 +0200

    ajout enregistrement de fichier

--- AspMVC/Roomy/Roomy/Areas/BackOffice/Controllers/RoomsController.cs
+++ AspMVC/Roomy/Roomy/Areas/BackOffice/Controllers/RoomsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -128,6 +129,26 @@ namespace Roomy.Areas.BackOffice.Controllers
             db.Rooms.Remove(room);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult AddFile(int id, HttpPostedFileBase upload)
+        {
+            var model = new RoomFile();
+
+            model.RoomID = id;
+            model.Name = upload.FileName;
+            model.ContentType = upload.ContentType;
+
+            using (var reader = new BinaryReader(upload.InputStream))
+            {
+                model.Content = reader.ReadBytes(upload.ContentLength);
+            }
+
+            db.RoomFiles.Add(model);
+            db.SaveChanges();
+
+            return RedirectToAction("Edit", new {id = model.RoomID });
         }
 
         protected override void Dispose(bool disposing)
